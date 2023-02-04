b2c43a3aa6946d7943bb9d9bfd2e52efdc86af84
Hilario Gutierrez <hilario.m.gutierrez@gmail.com>
2023-02-04 11:36:59 -0300

    se corrigio la url de item para ver el itemDetail

--- src/components/ItemDetailContainer/Index.jsx
+++ src/components/ItemDetailContainer/Index.jsx
@@ -4,38 +4,38 @@ import { useParams } from 'react-router-dom'
 import ItemDetail from '../ItemDetail/Index'
 
 const ItemDetailConteiner = () => {
-  const [data, setData] = useState([])
-  const {name} = useParams()
+  const [data, setData] = useState({})
+  const { name } = useParams()
 
   const dataJson = () => {
     axios
-      .get('/vinos.json') // hace peticion a api o json de datos
+      .get('../vinos.json') // hace peticion a api o json de datos
       .then((res) => {
         // obtenemos los datos
-                const data = res.data;
+        const data = res.data;
         // filtramos por id
-                const dataFiltrada = data.find(el => el.nombre === name)
+        const dataFiltrada = data.find(el => el.nombre === name)
         // actualizamos el estado con esa info
-                setData(dataFiltrada) 
-              }) // obtiene respuesta y la muestra 
+        setData(dataFiltrada)
+      })
       .catch((err) => console.log(err)) // si hay un error en peticion arroja por consola el error
   }
 
-  useEffect(() => { 
-    dataJson(name)
-  }, []) 
-  // el UseEffect [] --> Para se ejecute cuando se monta el sitio. [prop1] --> En montaje y para cada cambio de la prop que se le de a State [prop1,prop2] --> Cuando cambia alguna de las 2 prop cambia el State
+  useEffect(() => {
+    dataJson()
+  }, [name])
+  // el UseEffect [] --> Para se ejecute cuando se monta el sitio. 
+  //[prop1] --> En montaje y para cada cambio de la prop que se le de a State 
+  //[prop1,prop2] --> Cuando cambia alguna de las 2 prop cambia el State
   // en las props de useEffect tienen que ser states
   //variable que guarda petision a datos JSON x axios
-  
-  console.log(data)
+
 
   return (
     <>
-    <ItemDetail data={data} />
+      <ItemDetail data={data} />
     </>
   )
 }
 
 export default ItemDetailConteiner
-
