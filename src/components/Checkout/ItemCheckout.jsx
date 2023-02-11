63fd72cc1c74256ee351e4cf3983957758cd79e2
Hilario Gutierrez <hilario.m.gutierrez@gmail.com>
2023-02-11 01:27:18 -0300

    checkout responsive

--- src/components/Checkout/ItemCheckout.jsx
+++ src/components/Checkout/ItemCheckout.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 import styles from './checkout.module.css'
 const ItemCheckout = ({ item }) => {
     return (
-        <Box display='flex' flexDirection=' row' alignItems='center' justifyContent='space-between' margin={5}>
+        <Box display='flex' flexDirection={{base:'column', lg:'row'}} w={{base:'100%',md:'100%', lg:'100%'}} alignItems='center' justifyContent='space-between' margin={0}>
             <Image className={styles.img} alt={item.nombre} src={item.img}></Image>
             <Box display='flex' flexDirection='column'>
                 <Heading fontSize='1rem' fontStyle='italic'>{item.nombre}</Heading>
-                <Box display='flex' flexDirection='row'>
+                <Box display='flex' justifyContent={{base:'center',lg:'center'}} flexDirection={{base:'column', lg:'row'}} w={{base:'100%',md:'100%', lg:'100%'}}>
                     <Text className={styles.texto}>Precio unitario: ${item.precio}</Text>
                     <Text className={styles.texto}>Cantidad: {item.cantidad}</Text>
                     <Text className={styles.texto}>Subtotal: ${item.cantidad * item.precio}</Text>
